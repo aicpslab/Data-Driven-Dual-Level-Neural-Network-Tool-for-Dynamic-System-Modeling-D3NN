57be1b4390113e3d44ab0f915913bf6e00671bab
Yangyejiang <51980560+Yangyejiang@users.noreply.github.com>
2024-01-21 18:55:51 -0500

    Delete @partitions directory

--- @partitions/CombineReach.m
+++
@@ -1,7 +0,0 @@
-function outputbound=CombineReach(inputbound)
-         for i = 1:size(inputbound,1)/2
-         outputbound(2*i-1,1) = min(inputbound(2*i-1,:));
-         outputbound(2*i,1) = max(inputbound(2*i,:));
-         end
-   
-end
